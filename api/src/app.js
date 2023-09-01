8d1569efb31000174dc648ac4c78ec7ee91066ae
nicoflorentin <nicoflorentn@gmail.com>
2023-09-01 15:11:28 -0300

    cors fix

--- api/src/app.js
+++ api/src/app.js
@@ -11,21 +11,16 @@ const server = express()
 
 server.name = "API"
 
-// server.use(
-// 	cors({
-// 		origin: ["http://localhost:3000", "https://pokemon-viewer-pi.vercel.app"],
-// 	})
-// )
+server.use(
+	cors({
+		origin: ["http://localhost:3000", "https://pokemon-viewer-pi.vercel.app"],
+	})
+)
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }))
 server.use(bodyParser.json({ limit: "50mb" }))
 server.use(cookieParser())
 server.use(morgan("dev"))
 server.use((req, res, next) => {
-	// res.header("Access-Control-Allow-Origin", "http://localhost:3000") // update to match the domain you will make the request from
-	res.header("Access-Control-Allow-Origin", ["http://localhost:3000", "https://pokemon-viewer-pi.vercel.app"]) // update to match the domain you will make the request from
-	res.header("Access-Control-Allow-Credentials", "true")
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 	next()
 })
 
