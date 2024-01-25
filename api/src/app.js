3447db303359dc9d73844bc1da398f948f161fb8
nicoflorentin <nicoflorentn@gmail.com>
2024-01-25 15:47:00 -0300

    cors fix

--- api/src/app.js
+++ api/src/app.js
@@ -18,7 +18,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 // CORS CONFIG
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://pokemon-viewer-pi.vercel.app/"],
+  origin: ["http://localhost:5173", "https://pokemon-viewer-pi.vercel.app"],
   credentials: true, //access-control-allow-credentials:true
   allowedHeaders: ["Content-Type", "Authorization"],
 };
