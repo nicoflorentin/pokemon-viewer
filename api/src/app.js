76a25db2da832889de5bfafbe814c9f04493146f
nicoflorentin <nicoflorentn@gmail.com>
2024-01-25 14:38:06 -0300

    cors fix

--- api/src/app.js
+++ api/src/app.js
@@ -14,13 +14,14 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://powemin-viewer-api.onrender.com/'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+//CORS CONFIG
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://powemin-viewer-api.onrender.com"],
+  credentials: true, //access-control-allow-credentials:true
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 
 server.use('/', routes);
 
