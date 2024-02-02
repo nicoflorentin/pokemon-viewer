80637de69e4dffbbae383580f6236e0e407a2f2b
nicoflorentin <nicogrunger@hotmail.com>
2024-02-02 16:57:30 -0300

    fix

--- client/src/components/Loader/Loader.jsx
+++ client/src/components/Loader/Loader.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-import style from "./Loader.module.css";
-const Loader = ({ image }) => {
-	return <img className={style.loader} src={image} alt="loader" />;
-};
+import React from "react"
+import style from "./Loader.module.css"
 
-export default Loader;
+const Loader = ({ image }) => {
+	return (
+		<>
+			<img className={style.img} src={image} alt="loader" />
+			<p>Loading might take approximately a minute due to server limitations. Please be patient...</p>
+		</>
+	)
+}
+
+export default Loader
